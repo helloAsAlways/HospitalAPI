552d9a7afd0a37054b2b0f789eeb6aa95ffec6c0
dam <helloAsAlways>
2026-07-25 16:27:05 +0800

    updating the model and controller

---
+++ Controllers/DoctorsController.cs
@@ -0,0 +1,12 @@
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+using Supabase;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers;
+
+public class DoctorController
+{
+    
+}
