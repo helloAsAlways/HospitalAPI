dae1d7f76065a5458a68523b2578d0efceff4831
dam <helloAsAlways>
2026-07-31 18:24:16 +0800

    final

--- obj/Debug/net9.0/WebApplication2.AssemblyInfo.cs
+++ obj/Debug/net9.0/WebApplication2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc4e78ec9482755d5f0b5578ddc5290d9b5335b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a1c0d088c0e1ffbdc48069417186a378a2dd4af")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
